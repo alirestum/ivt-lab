f572f5fc86ec0445efecc192bea4a2749ddedf1e
Ali Restum <alirestum.hu@gmail.com>
2021-05-01 23:05:03 +0200

    inlined random generation

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -39,9 +39,7 @@ public class TorpedoStore {
     boolean success = false;
 
     // simulate random overheating of the launcher bay which prevents firing
-    double r = generator.nextDouble();
-
-    if (r >= FAILURE_RATE) {
+    if (generator.nextDouble() >= FAILURE_RATE) {
       // successful firing
       this.torpedoCount -= numberOfTorpedos;
       success = true;
